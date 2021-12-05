0459009d7b1f270fa45784362eceeb4829da2da3
Vaibhav Mishra <mishravaibhav9212@gmail.com>
2021-12-05 18:57:16 +0530

    header completed

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,13 @@
-// alert('JS')
+const toggleBar = () => {
+
+    var toggleMenu = document.getElementsByClassName('nav')[0];
+
+    if (toggleMenu.style.height == '12vh') {
+        toggleMenu.style.height = '40rem'
+    } else {
+        toggleMenu.style.height = '12vh'
+    }
+
+}
+
+
