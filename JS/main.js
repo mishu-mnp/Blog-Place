0401f8d63c06d92103d910d5c49b45cf6f33e77f
Vaibhav Mishra <mishravaibhav9212@gmail.com>
2021-12-07 19:24:51 +0530

    bloog completed

--- JS/main.js
+++ JS/main.js
@@ -2,10 +2,10 @@ const toggleBar = () => {
 
     var toggleMenu = document.getElementsByClassName('nav')[0];
 
-    if (toggleMenu.style.height == '12vh') {
-        toggleMenu.style.height = '40rem'
+    if (toggleMenu.style.height == '10vh') {
+        toggleMenu.style.height = '30rem'
     } else {
-        toggleMenu.style.height = '12vh'
+        toggleMenu.style.height = '10vh'
     }
 
 }
